2ab2d3cc90fc6b24dfaf89c2215b9a312c156a0d
effinmr <effinmr@gmail.com>
2025-06-13 15:12:02 +0530

    ren

--- app/src/main/java/code/name/monkey/retromusic/glide/artistimage/ArtistImageFetcher.kt
+++ app/src/main/java/code/name/monkey/retromusic/glide/artistimage/ArtistImageFetcher.kt
@@ -42,7 +42,7 @@ class ArtistImageFetcher(
     override fun loadData(priority: Priority, callback: DataFetcher.DataCallback<in InputStream>) {
         try {
             if (!MusicUtil.isArtistNameUnknown(model.artist.name) &&
-                PreferenceUtil.isOnlineMode &&
+                (!PreferenceUtil.isOfflineMode) &&
                 PreferenceUtil.isAllowedToDownloadMetadata(context)
             ) {
                 val artists = model.artist.name.split(",", "&")
