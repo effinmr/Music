904bef5c606912c380d6531043d659ef759ac9b4
effinmr <effinmr@gmail.com>
2025-06-16 01:56:53 +0530

    Update SongRepository.kt

--- app/src/main/java/code/name/monkey/retromusic/repository/SongRepository.kt
+++ app/src/main/java/code/name/monkey/retromusic/repository/SongRepository.kt
@@ -170,15 +170,7 @@ class RealSongRepository(private val context: Context) : SongRepository {
         val composer = cursor.getStringOrNull(AudioColumns.COMPOSER)
         val albumArtist = cursor.getStringOrNull("album_artist")
 
-        val year = try {
-            val retriever = MediaMetadataRetriever()
-            retriever.setDataSource(data)
-            val date = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DATE)
-            retriever.release()
-            date?.takeIf { it.isNotBlank() }
-        } catch (e: Exception) {
-            cursor.getInt(AudioColumns.YEAR).toString()
-        }
+        val year = cursor.getInt(AudioColumns.YEAR)
 
         return Song(
             id,
