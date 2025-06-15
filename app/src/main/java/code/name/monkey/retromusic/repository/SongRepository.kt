482520dc0fbf255b229baaf4ea852eb8e1878ff9
effinmr <effinmr@gmail.com>
2025-06-16 04:32:19 +0530

    fix get year from metadata

--- app/src/main/java/code/name/monkey/retromusic/repository/SongRepository.kt
+++ app/src/main/java/code/name/monkey/retromusic/repository/SongRepository.kt
@@ -186,7 +186,7 @@ class RealSongRepository(private val context: Context) : SongRepository {
             retriever.release()
             date?.takeIf { it.isNotBlank() }
         } catch (e: Exception) {
-            cursor.getInt(AudioColumns.YEAR).toString()
+            cursor.getStringOrNull(AudioColumns.YEAR).toString()
         }
 
         return Song(
