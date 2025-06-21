80c8fa488827a97592daddd7ac766b831ca2b698
effinmr <effinmr@gmail.com>
2025-06-21 16:59:47 +0530

    fix cover

--- app/src/main/java/code/name/monkey/retromusic/adapter/artist/ArtistAdapter.kt
+++ app/src/main/java/code/name/monkey/retromusic/adapter/artist/ArtistAdapter.kt
@@ -105,7 +105,6 @@ class ArtistAdapter(
             holder.image?.isVisible = false
             holder.imageText?.isVisible = true // Show imageText if not showing cover
             holder.imageText?.text = (position + 1).toString() // Display song number
-            holder.imageTextContainer?.isVisible = false
         }
     }
 
