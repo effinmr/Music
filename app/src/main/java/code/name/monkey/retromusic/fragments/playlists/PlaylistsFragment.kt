0856f5a2fdd09df1604a69b0cd5d6db22b8bffc6
effinmr <effinmr@gmail.com>
2025-06-30 00:28:05 +0530

    add orderable playlists

--- app/src/main/java/code/name/monkey/retromusic/fragments/playlists/PlaylistsFragment.kt
+++ app/src/main/java/code/name/monkey/retromusic/fragments/playlists/PlaylistsFragment.kt
@@ -61,6 +61,26 @@ class PlaylistsFragment :
             else
                 adapter?.swapDataSet(listOf())
         }
+        val itemTouchHelper = ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(
+            ItemTouchHelper.UP or ItemTouchHelper.DOWN, 0) {
+
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                val fromPosition = viewHolder.adapterPosition
+                val toPosition = target.adapterPosition
+                adapter?.swapItems(fromPosition, toPosition)
+                adapter?.notifyItemMoved(fromPosition, toPosition)
+                return true
+            }
+            
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                // No swipe action
+            }
+        })
+        itemTouchHelper.attachToRecyclerView(recyclerView)
     }
 
     override val titleRes: Int
