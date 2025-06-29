0cef31cf335fa69aaeebdbfa5e4656eeb79d9ea3
effinmr <effinmr@gmail.com>
2025-06-29 09:48:15 +0530

    fix year

---
+++ app/src/main/java/code/name/monkey/retromusic/repository/MetadataScanner.kt
@@ -0,0 +1,40 @@
+package code.name.monkey.retromusic.repository
+
+import android.content.Context
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.metadata.Metadata
+import com.google.android.exoplayer2.metadata.MetadataOutput
+import com.google.android.exoplayer2.metadata.id3.Id3Frame
+import com.google.android.exoplayer2.metadata.id3.TextInformationFrame
+
+class MetadataScanner(private val context: Context) {
+
+    interface ScanListener {
+        fun onDateScanned(path: String, recordDate: String?)
+    }
+
+    fun scan(path: String, listener: ScanListener) {
+        val player = ExoPlayer.Builder(context).build()
+
+        player.addMetadataOutput(object : MetadataOutput {
+            override fun onMetadata(metadata: Metadata) {
+                for (i in 0 until metadata.length()) {
+                    val entry = metadata[i]
+                    if (entry is Id3Frame && (entry.id == "TDRC" || entry.id == "TYER")) {
+                        val date = (entry as TextInformationFrame).value
+                        player.release()
+                        listener.onDateScanned(path, date)
+                        return
+                    }
+                }
+                player.release()
+                listener.onDateScanned(path, null)
+            }
+        })
+
+        player.setMediaItem(MediaItem.fromUri(path))
+        player.prepare()
+        player.playWhenReady = false
+    }
+}
