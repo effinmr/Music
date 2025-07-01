8fef769d5a4ba6c9bd21ca43d5ae30aef9114aa0
effinmr <effinmr@gmail.com>
2025-07-01 09:44:38 +0530

    fix migration
    fix migration
    
    update migrations
    
    update migrations

--- app/src/main/java/code/name/monkey/retromusic/db/RoomMigrations.kt
+++ app/src/main/java/code/name/monkey/retromusic/db/RoomMigrations.kt
@@ -7,6 +7,40 @@ val MIGRATION_23_24 = object : Migration(23, 24) {
     override fun migrate(database: SupportSQLiteDatabase) {
         database.execSQL("DROP TABLE LyricsEntity")
         database.execSQL("DROP TABLE BlackListStoreEntity")
+    }
+}
+
+val MIGRATION_25_26 = object : Migration(25, 26) {
+    override fun migrate(database: SupportSQLiteDatabase) {
         database.execSQL("ALTER TABLE PlaylistEntity ADD COLUMN position INTEGER NOT NULL DEFAULT 0")
     }
 }
+
+val MIGRATION_26_27 = object : Migration(26, 27) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+        database.execSQL("""
+            CREATE TABLE IF NOT EXISTS song_metadata (
+                id INTEGER PRIMARY KEY NOT NULL,
+                title TEXT,
+                trackNumber INTEGER,
+                year TEXT,
+                duration INTEGER,
+                data TEXT,
+                dateModified INTEGER,
+                artistId INTEGER,
+                albumId INTEGER,
+                albumName TEXT,
+                artistName TEXT,
+                composer TEXT,
+                albumArtist TEXT,
+                allArtists TEXT
+            )
+        """.trimIndent())
+    }
+}
+
+val allMigrations = arrayOf(
+    MIGRATION_23_24,
+    MIGRATION_25_26,
+    MIGRATION_26_27
+)
