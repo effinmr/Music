dd13ccef0445fdec18d0549cbd850f216a74eae7
effinmr <effinmr@gmail.com>
2025-07-01 09:55:11 +0530

    fix migration

--- app/src/main/java/code/name/monkey/retromusic/db/RoomMigrations.kt
+++ app/src/main/java/code/name/monkey/retromusic/db/RoomMigrations.kt
@@ -7,6 +7,16 @@ val MIGRATION_23_24 = object : Migration(23, 24) {
     override fun migrate(database: SupportSQLiteDatabase) {
         database.execSQL("DROP TABLE LyricsEntity")
         database.execSQL("DROP TABLE BlackListStoreEntity")
+    }
+}
+
+val MIGRATION_25_26 = object : Migration(25, 26) {
+    override fun migrate(database: SupportSQLiteDatabase) {
         database.execSQL("ALTER TABLE PlaylistEntity ADD COLUMN position INTEGER NOT NULL DEFAULT 0")
     }
 }
+
+val allMigrations = arrayOf(
+    MIGRATION_23_24,
+    MIGRATION_25_26
+)
